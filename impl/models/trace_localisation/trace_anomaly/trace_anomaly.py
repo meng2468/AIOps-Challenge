f3b5fc76d38efc3397a669b79f6e206be67864ac
TiagoMAntunes <tiago.melo.antunes@gmail.com>
2020-12-06 20:28:02 +0000

    Started implementing TraceAnomaly

---
+++ impl/models/trace_localisation/trace_anomaly/trace_anomaly.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import torch.functional as F
+
+
+class TraceAnomalyDetector(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+
+class TraceAnomaly:
+    def __init__(self, dimension):
+        pass
---
+++ impl/models/trace_localisation/trace_anomaly/utils.py
@@ -0,0 +1,36 @@
+from itertools import groupby
+
+def trace_to_call_path(df):
+    """
+    df : Trace Pandas dataframe. The dataframe must be sorted and pre processed
+    Pre processing includes dsName being removed from JDBC and LOCAL
+    """
+    print(df)
+
+    # transform CSF serviceName
+    ids = df[df['callType'] == 'CSF']['id']
+    children_cmdb = df[df['pid'].isin(ids)]['cmdb_id']
+    df.loc[df['callType']=='CSF', 'serviceName'] = list(children_cmdb)
+
+
+    names = dict(zip(df.id, df.serviceName))
+    names['None'] = 'Start'
+
+    path = []
+
+    def merge_fn(row):
+        """ Makes a row become (pid, id) format while filtering same service calls"""
+        v = (names[row['pid']], names[row['id']])
+        if v[0] == v[1]:
+            return
+        path.append(v)
+    
+    df.apply(merge_fn, axis=1) # apply horizontally
+    l = path
+    path = [x[0] for x in groupby(path)]
+    times = df['elapsedTime'].values.tolist()
+    call_path = set()
+    for index, p in enumerate(path):
+        call_path.add((p[1], tuple(path[:index + 1]), times[index]))
+
+    return call_path
