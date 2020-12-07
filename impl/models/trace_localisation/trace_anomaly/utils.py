93d36bcc30bc3a196ab41a4716b796798401bd13
TiagoMAntunes <tiago.melo.antunes@gmail.com>
2020-12-07 07:45:15 +0000

    Model now summing times instead of losing them

--- impl/models/trace_localisation/trace_anomaly/utils.py
+++ impl/models/trace_localisation/trace_anomaly/utils.py
@@ -16,20 +16,27 @@ def trace_to_call_path(df):
     names['None'] = 'Start'
 
     path = []
-
+    durations = []
     def merge_fn(row):
         """ Makes a row become (pid, id) format while filtering same service calls"""
         v = (names[row['pid']], names[row['id']])
         if v[0] == v[1]:
+            durations[-1] += row['elapsedTime']
             return
+        else:
+            durations.append(row['elapsedTime'])
         path.append(v)
     
     df.apply(merge_fn, axis=1) # apply horizontally
     l = path
-    path = [x[0] for x in groupby(path)]
-    times = df['elapsedTime'].values.tolist()
-    call_path = set()
-    for index, p in enumerate(path):
-        call_path.add((p[1], tuple(path[:index + 1]), times[index]))
+    
+    # reduce repeated duplicates and make sure their times are saved
+    tmp = [(x[0], sum([1 for _ in x[1]])) for x in groupby(path)]
+    times = list(map(lambda x: x[1], tmp))
+    path = list(map(lambda x: x[0], tmp))
 
+    call_path = set()
+    # generate path, last sum is the sum of repeated times
+    for index, p in enumerate(path):
+        call_path.add((p[1], tuple(path[:index + 1]), sum(durations[index:times[index] + index])))
     return call_path
